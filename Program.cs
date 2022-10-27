91800ee0f490c40b15e24a377e3167af9fe805a2
Ali Kızıldaş <ali.kizildas@gmail.com>
2022-10-27 12:26:47 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayi giriniz.");
+            UcgenCiz(int.Parse(Console.ReadLine()));
+        }
+
+        public static void UcgenCiz(int a)
+        {
+            for (int i = 0; i <2*a; i++)
+            {                
+                for (int n = 0; n <2*a-i; n++)
+                {
+                    Console.Write(" ");
+                }
+                             
+                for (int j = 0; j <2*i; j++)
+                {
+                    Console.Write("*");
+                }                               
+                Console.WriteLine("");           
+            }
+        }
+    }
+}
